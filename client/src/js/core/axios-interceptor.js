bba5a5dffdd4c0ecb2f83d7892bd2ea5ce98f746
Alex Hampu <alex.hampu@deploy.com.ro>
2022-09-02 14:45:10 +0300

    remove console.log

--- client/src/js/core/axios-interceptor.js
+++ client/src/js/core/axios-interceptor.js
@@ -3,8 +3,6 @@ import {interceptors} from "axios";
 const isAbsoluteURLRegex = /^(?:\w+:)\/\//;
 
 interceptors.request.use(function(config) {
-    console.log({config});
-
     if ( !isAbsoluteURLRegex.test(config.url) ) {
         config.url = [import.meta.env.VITE_API_URL, config.url].join(config.url.startsWith('/') ? '' : '/');
     }
