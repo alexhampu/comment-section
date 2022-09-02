b061d90ccf40a68037b2605b80c7599b918f6e78
Alex Hampu <alex.hampu@deploy.com.ro>
2022-09-02 16:17:57 +0300

    refactor comment mapper

--- server/src/services/comment.service.js
+++ server/src/services/comment.service.js
@@ -1,18 +1,25 @@
 const CommentRepository = require('../repositories/comment.repository');
 
+const commentMapper = async (comments) => {
+    return await Promise.all(comments.map(async (comment) => {
+        const upvote = await CommentRepository.getUpvoteByCommentAndAuthor(comment.id, 1);
+
+        comment.isUpvoted = upvote !== null;
+        comment.replies = await commentMapper(comment.replies ?? []);
+
+        return comment;
+    }));
+}
+
+async function store(authorId, content, parentId) {
+    let comment = await CommentRepository.store(authorId, content, parentId);
+    comment = await commentMapper([comment]);
+
+    return comment.shift();
+}
+
 async function getAll() {
     const comments = await CommentRepository.getAll();
-
-    const commentMapper = async (comments) => {
-        return await Promise.all(comments.map(async (comment) => {
-            const upvote = await CommentRepository.getUpvoteByCommentAndAuthor(comment.id, 1);
-
-            comment.isUpvoted = upvote !== null;
-            comment.replies = await commentMapper(comment.replies ?? []);
-
-            return comment;
-        }));
-    }
 
     return commentMapper(comments);
 }
