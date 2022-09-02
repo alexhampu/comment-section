52fb0aae7f176a2742e4e5485b6159fb7550e963
Alex Hampu <alex.hampu@deploy.com.ro>
2022-09-02 12:24:27 +0300

    add client UI

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: 2
+    },
+    screens: {
+      xl: '800px',
+    },
+    extend: {
+      colors: {
+        primary: '#7e34f6'
+      }
+    },
+  },
+  plugins: [],
+}
