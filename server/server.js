e682aa50f9879b50f2130c2ccc7cfd491ef7b569
Alex Hampu <alex.hampu@deploy.com.ro>
2022-09-02 19:11:42 +0300

    add socket.io

--- server/server.js
+++ server/server.js
@@ -2,16 +2,27 @@ require('dotenv').config();
 
 const cors = require('cors');
 const express = require('express');
+const { Server } = require("socket.io");
 const apiRoutes = require('./src/routes/api');
-const {APP_PORT = 4201, APP_HOSTNAME = 'localhost'} = process.env;
+const {WS_PORT = 4200, APP_PORT = 4201, APP_HOSTNAME = 'localhost'} = process.env;
 const URL = `http://${APP_HOSTNAME}:${APP_PORT}`;
 const app = express();
-
-app.listen(APP_PORT, () => {
-    console.log(`Server is online, visit at: ${URL}`);
-});
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/api', apiRoutes);
+
+const io = new Server({
+    cors: {
+        origin: "*"
+    }
+});
+
+io.listen(WS_PORT);
+
+app.set('io', io);
+
+app.listen(APP_PORT, () => {
+    console.log(`Server is online, visit at: ${URL}`);
+});
