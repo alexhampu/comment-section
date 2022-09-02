e84f66b44aff179ed983c992880026207e748789
Alex Hampu <alex.hampu@deploy.com.ro>
2022-09-02 17:15:32 +0300

    remove unused script

--- client/src/js/script.js
+++
@@ -1,9 +0,0 @@
-import './core/axios-interceptor.js';
-import {createComments, getLeaveCommentElement} from "./core/templates.js";
-import CommentService from "./services/comment.service.js";
-
-document.querySelector('.CommentSection__Header').appendChild(getLeaveCommentElement());
-
-CommentService.getAll().then((comments) => {
-    createComments(comments, document.querySelector('.CommentSection__List'));
-});
